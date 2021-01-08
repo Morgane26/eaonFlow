f96209cbafef41575006d6c50cc15351e83daadb
Morgane Perenzoni <mperenzoni@gmail.com>
2021-01-08 18:29:28 +0100

    add first api

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,12 +1,26 @@
-import './Cards.css'
+import './Cards.css';
 
 import React, { Component } from 'react';
 
 class Cards extends Component {
+    state = {
+        post: ""
+    }
+    componentDidMount () {
+        fetch("https://smart-port-data-ampmetropole.opendatasoft.com/api/v2/catalog/datasets?apikey=" + process.env.REACT_APP_API_KEY)
+            .then((response) => {
+                return response.json()
+            })
+            .then ((result) => {
+                this.setState({post: result})
+                console.log(result)
+            })
+    }
     render () {
         return (
             <div className="cards">
                 <div className="card-one"/>
+                {this.setState.post}
                     <div className="box-of-two-cards">
                         <div className="card-two"/>
                             <div className="card-three"/>
@@ -14,8 +28,7 @@ class Cards extends Component {
                         <div className="card-four"/>
             </div>
         )
-        }
+    }
 }
 export default Cards
-
 
